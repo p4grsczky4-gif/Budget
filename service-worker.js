a0be7b1e86be513df445e00d89624cf5bac95974
p4grsczky4-gif <p4grsczky4@privaterelay.appleid.com>
2026-07-26 12:21:30 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-const C="budsjett-v2",F=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="budsjett-v3",F=["./","./index.html","./app-info.json","./icon-192.png","./icon-512.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
